60d3b957ebc9999a91d86372f4c1414191f17712
saisowri <144709472+saisowri@users.noreply.github.com>
2023-09-11 21:18:06 +0530

    day1demo
    demo

--- Day1demo.java
+++ Day1demo.java
@@ -4,7 +4,9 @@ public class Day1demo {
 
 	public static void main(String[] args) {
 		System.out.println("hai sowri welcome to selenium");
-		// TODO Auto-generated method stub
+		int x = 5;
+		System.out.println(x);
+		
 
 	}
 
