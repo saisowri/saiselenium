bd5ef39f9f5605f46e11b6e0f446e687f852a0dd
saisowri <144709472+saisowri@users.noreply.github.com>
2023-09-11 21:14:11 +0530

    day1.assignment
    Incomplete

---
+++ Day1demo.java
@@ -0,0 +1,11 @@
+package day1.sample;
+
+public class Day1demo {
+
+	public static void main(String[] args) {
+		System.out.println("hai sowri welcome to selenium");
+		// TODO Auto-generated method stub
+
+	}
+
+}
