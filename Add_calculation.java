c1f9b102cf422f6803f4baaadb951fa0465f4bbb
saisowri <144709472+saisowri@users.noreply.github.com>
2023-09-12 18:57:43 +0530

    Addition_arithmetic and datatypes project updated
    please check in this project

---
+++ Add_calculation.java
@@ -0,0 +1,41 @@
+package add_day1proj;
+
+public class Add_calculation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=2;
+		int plus=a+b;
+		int minus=a-b;
+		double d=2.5;
+		float f=12;
+		char c='c';
+		boolean comment=true;
+		String proj="ARITHMETIC";
+		System.out.println("project name is :"+proj);
+		System.out.println("addition is :"+plus);
+		System.out.println("subtraction is :"+minus);
+		System.out.println("multiplication is :"+a*b);
+		System.out.println("division is :"+a/b);
+		System.out.println("modulo is:"+a%b);
+		System.out.println("Data Types:");
+		System.out.println("int:"+a);
+		System.out.println("double:"+d);
+		System.out.println("float:"+f);
+		System.out.println("char:"+c);
+		System.out.println("boolean:"+comment);
+		System.out.println("string:"+proj);
+		
+		
+		
+		
+		
+		
+		
+				
+				
+
+	}
+
+}
