7154d87af91795cf37113546ba00cd8029e4fcf5
saisowri <144709472+saisowri@users.noreply.github.com>
2023-09-13 13:50:06 +0530

    ASSIGNMENT FOR iphone_project
    updated with Iphone_task

---
+++ Iphone.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Iphone {
+	public void makecall() {
+		System.out.println("call to 2:30 pm \ncall to 4:00pm");
+	}
+	public void model() {
+		System.out.println("phone model name:\nSamsung\nOppo");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Smartphone.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class Smartphone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Iphone ip=new Iphone();
+		ip.makecall();
+		ip.model();
+
+	}
+
+}
